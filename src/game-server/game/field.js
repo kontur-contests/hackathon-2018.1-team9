c904e9b8a2d905e731d610eaa082fafe13709df1
Eugene Churmanov <echurmanov@yandex-team.ru>
2018-04-07 15:29:30 +0500

    Методы заполнения поля по дропам

--- src/game-server/game/field.js
+++ src/game-server/game/field.js
@@ -1,22 +1,59 @@
 const Cell = require('./cell.js');
 
-
 class Field {
     constructor (width = 9, height = 9) {
         this.width = width;
         this.height = height;
 
         this.cells = [];
+        this.freeCells = 0;
 
         for (let x = 0; x < 9; x++) {
             this.cells[x] = [];
             for (let y = 0; y < 9; y++) {
                 this.cells[x][y] = new Cell(this, x, y);
+                this.freeCells += 1;
             }
         }
+
+        this.nextDrop = 0;
     }
 
     findWay(from, to) {
 
     }
+
+    getLines() {
+
+    }
+
+    /**
+     * Возвращает координаты клетки куда помещен "шар" из сброса
+     *
+     * @param ball
+     * @param number
+     *
+     * @return {{x: number, y: number}}
+     */
+    placeDrop(ball, number) {
+        let normalNumber = number % this.freeCells;
+        let x = 0;
+        let y = 0;
+
+        for (x = 0; x < this.width && normalNumber; x++) {
+            for (y = 0; y < this.height && normalNumber; y++) {
+                if (!this.cells[x][y].ball) {
+                    normalNumber -= 1;
+                }
+            }
+        }
+
+        this.cells[x][y].ball = ball;
+
+        return {x, y};
+    }
 }
+
+modules.exports = {
+    Field
+};
---
+++ src/game-server/game/game.js
@@ -0,0 +1,78 @@
+const {Field} = require('./field.js');
+const {Ball, COLORS} = require('./ball.js');
+
+const DROP_COLORS = [
+    COLORS.BLUE, COLORS.CYAN, COLORS.GREEN, COLORS.MAGENTA, COLORS.RED, COLORS.YELLOW
+];
+
+class Game {
+    constructor(dropSize, targetScore, fieldSize){
+        this.dropSize = dropSize;
+        this.targetScore = targetScore;
+        this.fields = [
+            new Field(fieldSize, fieldSize),
+            new Field(fieldSize, fieldSize)
+        ];
+
+        this.fieldSize = fieldSize;
+
+        this.dropColors = [];
+        this.dropPositions = [];
+    }
+
+    getDropColors(dropNumber) {
+        while (this.dropColors.length < dropNumber - 1) {
+            const drop = [];
+
+            for (let i = 0; i < this.dropSize; i++) {
+                drop.push(DROP_COLORS[Math.floor(Math.random() * DROP_COLORS.length)]);
+            }
+
+            this.dropColors.push(drop);
+        }
+
+        return this.dropColors(dropNumber);
+    }
+
+    getDropPositions (dropNumber) {
+        while (this.dropPositions.length < dropNumber - 1) {
+            const drop = [];
+
+            for (let i = 0; i < this.dropSize; i++) {
+                drop.push(Math.floor(Math.random() * this.fieldSize * this.fieldSize));
+            }
+
+            this.dropPositions.push(drop);
+        }
+
+        return this.dropPositions(dropNumber);
+    }
+
+    /**
+     *
+     * @param {Field} field
+     * @returns {Array}
+     */
+    doDropToField(field) {
+        const dropColors = this.getDropColors(field.nextDrop);
+        const dropPositions = this.getDropPositions(field.nextDrop);
+
+        const dropResult = [];
+
+        for (let p = 0; p < dropPositions.length; p++) {
+            const ball = new Ball(dropColors[p]);
+
+            const {x, y} = field.placeDrop(ball, dropPositions[p]);
+
+            dropResult.push({position: {x, y}, ball});
+        }
+
+        field.nextDrop += 1;
+
+        return dropResult;
+    }
+}
+
+module.exports = {
+    Game
+};
